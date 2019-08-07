2bf244768d37560ad82adfd03fcacfe00a1a7fe5
hossay <youhs4554@gmail.com>
2019-08-07 20:17:17 +0900

    add /api uri, hello api

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('api', views.hello_api, name='hello_api')
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,12 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+
+# myapp/views.py
+from django.http import JsonResponse
+
+def hello_api(request):
+    return JsonResponse({
+        'title' : 'Hello API!'},
+        json_dumps_params = {'ensure_ascii': True})
